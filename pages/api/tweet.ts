7fc4c605ba6b87f86aa6f86e7bc1f839aec6dff1
Galeno de Melo <galenodemelo@gmail.com>
2022-06-27 15:13:46 -0300

    refactor: Tweetando apenas uma frase para não exceder o limite de caracteres

--- pages/api/tweet.ts
+++ pages/api/tweet.ts
@@ -1,11 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import config from "../../settings/config"
 import GeradorLeroLero from "../../src/vendor/GeradorLeroLero"
 
 export default function Tweet(req: NextApiRequest, res: NextApiResponse) {
     const generator: GeradorLeroLero = new GeradorLeroLero()
-    const phraseCount: number = Math.round(Math.random() * (config.geradorLeroLero.phrases.max - config.geradorLeroLero.phrases.min) + config.geradorLeroLero.phrases.min)
 
-    const tweet: string = [...Array(phraseCount)].map(() => generator.build()).join(" ")
+    const tweet: string = generator.build() + "."
     res.status(200).json(tweet)
 }
